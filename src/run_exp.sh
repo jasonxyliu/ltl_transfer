5ea8a495ad4a0b90f232259c5b845b829e8f06d0
jasonxyliu <xinyu_liu@brown.edu>
2021-12-18 01:11:58 -0500

    bash to run on grid

---
+++ src/run_exp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --mem=3G
+#SBATCH -t 24:00:00
+#SBATCH --array=0-29
+
+# Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
+#SBATCH -e sbatch_out/arrayjob-%A-%a.err
+#SBATCH -o sbatch_out/arrayjob-%A-%a.out
+
+# Convert 1D indexing to 2D
+i=`expr $SLURM_ARRAY_TASK_ID % 3`
+j=`expr $SLURM_ARRAY_TASK_ID / 3`
+k=`expr $j % 2`
+l=`expr $j / 2`
+
+algo="lpopl"
+tasks=( "sequence" "interleaving" "safety" )
+task=${tasks[$i]}
+maps=( 0 1 2 3 4 5 6 7 8 9)
+map=${maps[$k]}
+
+# export PATH=/users/xliu53/anaconda/ltl_pomdp/bin:$PATH
+module load anaconda/3-5.2.0
+source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
+conda activate lpopl
+python3 run_experiments.py --algorithm=$algo --tasks=$task $j --map=$map
