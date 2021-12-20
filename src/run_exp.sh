f939fa671b23819efd52cb9db6ffd8df46a238d3
jasonxyliu <xinyu_liu@brown.edu>
2021-12-20 16:05:33 -0500

    loop over baseline algos

--- src/run_exp.sh
+++ src/run_exp.sh
@@ -2,7 +2,7 @@
 #SBATCH -n 1
 #SBATCH --mem=3G
 #SBATCH -t 24:00:00
-#SBATCH --array=0-29
+#SBATCH --array=0-89
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e sbatch_out/arrayjob-%A-%a.err
@@ -11,14 +11,16 @@
 # Convert 1D indexing to 2D
 i=`expr $SLURM_ARRAY_TASK_ID % 3`
 j=`expr $SLURM_ARRAY_TASK_ID / 3`
-k=`expr $j % 10`
-l=`expr $j / 10`
+k=`expr $j % 3`
+l=`expr $j / 3`
+m=`expr $l % 10`
 
-algo="lpopl"
+algos=( "dqn-l" "hrl-e" "hrl-l" )
+algo=${algos[$i]}
 tasks=( "sequence" "interleaving" "safety" )
-task=${tasks[$i]}
+task=${tasks[$k]}
 maps=( 0 1 2 3 4 5 6 7 8 9)
-map=${maps[$k]}
+map=${maps[$m]}
 
 # export PATH=/users/xliu53/anaconda/lpopl/bin:$PATH
 module load anaconda/3-5.2.0
