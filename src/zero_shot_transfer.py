7de93b0dc29b9fc1ad4b31ad89c95c2feb03b37f
ajshah91 <ajshah@mit.edu>
2022-03-10 17:14:41 -0500

    bug fix

--- src/zero_shot_transfer.py
+++ src/zero_shot_transfer.py
@@ -148,6 +148,7 @@ def relabel_cluster(tester, saver, curriculum, run_id, policy_bank, n_rollouts=1
 
 def run_single_worker_cluster(algo, task_id, map_id, run_id, ltl_id, state_id, n_rollouts, max_depth):
     import os
+    classifier_dpath = os.path.join("../tmp/", "task_%d/map_%d" % (tasks_id, map_id), "classifier")
     print(f'''Trying to run single worker rollout with following arguments:
             algo: {algo}
             run_id: {run_id}
@@ -155,7 +156,7 @@ def run_single_worker_cluster(algo, task_id, map_id, run_id, ltl_id, state_id, n
             state_id: {state_id}
             n_rollouts: {n_rollouts}
             max_depth: {max_depth}''')
-    classifier_dpath = os.path.join("../tmp/", "task_%d/map_%d" % (args.tasks_id, args.map_id), "classifier")
+
     from run_single_worker import single_worker_rollouts
     print(single_worker_rollouts)
     return 0
