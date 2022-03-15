0997650f23db39863281f614874918e361f240f7
jasonxyliu <xinyu_liu@brown.edu>
2022-03-15 14:57:37 -0400

    rename
    run_single_worker.py: v1
    run_single_worker.py: v2; not add loc violating edge constraint in init set

--- src/run_single_worker_v2.py
+++ src/run_single_worker_v2.py
@@ -1,4 +1,5 @@
 import os
+import time
 import dill
 import argparse
 from collections import defaultdict
@@ -25,20 +26,22 @@ def initialize_policy_bank(sess, task_aux, tester):
 
 def single_worker_rollouts(alg_name, classifier_dpath, run_id, ltl_id, state_id, n_rollouts, max_depth):
     """
-    Rollout a trained state-centric policy from init_state to see which outgoing edge it satisfies
+    Rollout a trained state-centric policy 'ltl_id' from init_state 'state_id' to target each outgoing edge
+    If start at a location violating edge constraint, this location will not be in initiation set of edge-centric option
     """
+    time_init = time.time()
     # load tester
     with open(os.path.join(classifier_dpath, "tester.pkl"), "rb") as file:
         tester = dill.load(file)
     saver = Saver(alg_name, tester)
     loader = Loader(saver)
-    # print("policy_dpath in worker: ", loader.saver.policy_dpath)
+    print("policy_dpath in worker: ", loader.saver.policy_dpath)
 
     # load init_state
     with open(os.path.join(classifier_dpath, "states.pkl"), "rb") as file:
         id2state = dill.load(file)
     init_state = id2state[state_id]
-    # print("init_state: ", init_state, state_id)
+    print("init_state: ", init_state, state_id)
 
     # create task_aux
     task_aux = Game(tester.get_task_params(tester.get_LTL_tasks()[0]))
@@ -47,60 +50,71 @@ def single_worker_rollouts(alg_name, classifier_dpath, run_id, ltl_id, state_id,
     config = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1, allow_soft_placement=True)
     with tf.Session(config=config) as sess:
         # load policy_bank
-        # print("loading policy bank")
+        print("loading policy bank")
         policy_bank = initialize_policy_bank(sess, task_aux, tester)
         loader.load_policy_bank(run_id, sess)
 
         id2ltl = {pid: policy for policy, pid in policy_bank.policy2id.items()}
         ltl = id2ltl[ltl_id]
-        # print("policy for ltl: ", ltl)
+        print("policy for ltl: ", ltl)
 
-        # run rollouts
-        edge2hits = rollout(tester, policy_bank, ltl, init_state, n_rollouts, max_depth)
+        edge2hits = {}
+        for edge_label in policy_bank.policies[ltl_id].get_edge_labels():
+            # run rollouts with a target edge
+            edge2hits[edge_label] = rollout(tester, policy_bank, ltl, init_state, edge_label, n_rollouts, max_depth)
+    print(edge2hits)
+    print("Rollout took: %0.2f mins\n" % ((time.time() - time_init)/60))
+
     # save rollout results
     saver.save_worker_results(run_id, ltl_id, init_state, edge2hits, n_rollouts)
 
 
-def rollout(tester, policy_bank, ltl, init_loc, n_rollouts, max_depth):
+def rollout(tester, policy_bank, ltl, init_loc, edge_label, n_rollouts, max_depth):
     """
-    Rollout trained policy from init_loc to see which outgoing edges it satisfies
+    Rollout trained policy 'ltl' from 'init_loc' to target a particular outgoing edge 'edge_label'
     """
-    edge2hits = defaultdict(int)
+    neg_props = [prop[1:] for prop in edge_label.split('&') if prop[0] == '!']
+    hits = 0
     task_aux = Game(tester.get_task_params(policy_bank.policies[policy_bank.get_id(ltl)].f_task, ltl))
     initial_state = task_aux.dfa.state  # get default DFA initial state before progressing on agent's init_loc
     for rollout in range(n_rollouts):
-        # print("\nrollout:", rollout)
-        # print("init_loc: ", init_loc)
-        # print("initial_state: ", initial_state)
+        print("\ntarget edge: ", edge_label)
+        print("rollout:", rollout)
+        print("init_loc: ", init_loc)
+        print("initial_state: ", initial_state)
 
         # Overwrite default agent start location and DFA initial state
         task = Game(tester.get_task_params(policy_bank.policies[policy_bank.get_id(ltl)].f_task, ltl, init_loc))
-        # print("cur DFA state: ", task.dfa.state)
-        # print("ltl: ", ltl)
-        # print("full ltl: ", policy_bank.policies[policy_bank.get_id(ltl)].f_task)
+        print("cur DFA state: ", task.dfa.state)
+        print("ltl: ", ltl)
+        print("full ltl: ", policy_bank.policies[policy_bank.get_id(ltl)].f_task)
 
         traversed_edge = None
         if initial_state != task.dfa.state:  # agent starts at a loc that already triggers a desired transition
             traversed_edge = task.dfa.nodelist[initial_state][task.dfa.state]
-            # print("traversed edge before while: ", traversed_edge)
+            print("traversed edge before while: ", traversed_edge)
         depth = 0
         while not traversed_edge and not task.ltl_game_over and not task.env_game_over and depth <= max_depth:
+            if task.get_true_propositions() in neg_props:  # proposition at cur location violates target edge constraint
+                break
             s1 = task.get_features()
             action = Actions(policy_bank.get_best_action(ltl, s1.reshape((1, len(task.get_features())))))
             prev_state = task.dfa.state
             _ = task.execute_action(action)
-            # print(prev_state, action, task.dfa.state)
+            print(prev_state, action, task.dfa.state)
             if prev_state != task.dfa.state:
                 traversed_edge = task.dfa.nodelist[prev_state][task.dfa.state]
-                # print("traversed edge  in while: ", traversed_edge)
+                print("traversed edge  in while: ", traversed_edge)
             depth += 1
         if traversed_edge:
             if traversed_edge not in policy_bank.policies[policy_bank.get_id(ltl)].get_edge_labels():
                 print("ERROR: policy %s traversed invalid outgoing edge %s from location %s" % (str(ltl), str(traversed_edge), str(init_loc)))
+            elif traversed_edge == edge_label:
+                hits += 1
             else:
-                edge2hits[traversed_edge] += 1
-    # print(edge2hits)
-    return edge2hits
+                print("policy %s traversed edge %s, not the target edge %s" % (str(ltl), str(traversed_edge), str(edge_label)))
+    print("hits: %d\n" % hits)
+    return hits
 
 
 if __name__ == "__main__":
@@ -114,7 +128,7 @@ if __name__ == "__main__":
     }  # for reference
 
     parser = argparse.ArgumentParser(prog="run_single_rollout", description='Rollout a trained policy from a given state.')
-    parser.add_argument('--algo', default='lpopl', type=str,
+    parser.add_argument('--algo', default='zero_shot_transfer', type=str,
                         help='This parameter indicated which RL algorithm to use. The options are: ' + str(algos))
     parser.add_argument('--tasks_id', default=4, type=int,
                         help='This parameter indicated which tasks to solve. The options are: ' + str(id2tasks.keys()))
@@ -122,9 +136,9 @@ if __name__ == "__main__":
                         help='This parameter indicated the ID of map to run rollouts')
     parser.add_argument('--run_id', default=0, type=int,
                         help='This parameter indicated the ID of the training run when models are saved')
-    parser.add_argument('--ltl_id', default=9, type=int,
+    parser.add_argument('--ltl_id', default=12, type=int,
                         help='This parameter indicated the ID of trained policy to rollout')
-    parser.add_argument('--state_id', default=180, type=int,
+    parser.add_argument('--state_id', default=237, type=int,
                         help='This parameter indicated the ID of state in which rollouts start')
     parser.add_argument('--n_rollouts', default=100, type=int,
                         help='This parameter indicated the number of rollouts')
