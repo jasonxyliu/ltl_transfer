e914a3023b7080a35c27a589d208f589f0c9ac58
ajshah91 <ankitshah91@gmail.com>
2022-03-21 17:09:06 -0400

    added formula sampler

--- src/formula_sampler.py
+++ src/formula_sampler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 def conjunctions(clauses):
     '''Combines all the clauses separated with 'and's. Note that this codebase only allows for
     binary conjunctions, and not a list of conjunctions as in the PUnS codebase'''
@@ -23,13 +25,35 @@ def soft_order(seq):
         return ('until', 'True', ('and', seq[0], soft_order(seq[1::])))
 
 def sample_waypoints(props):
-    a=1
+    visit_waypoints = []
+    for p in props:
+        if np.random.binomial(1,0.5):
+            visit_waypoints.append(p)
+    return visit_waypoints
 
 def sample_sequences(visit_waypoints):
-    a=1
+    
+    sequences = []
+    new_seq = [visit_waypoints[0]]
+        
+    for w in visit_waypoints[1::]:
+        if np.random.binomian(1,0.5):
+            #start a new sequence
+            sequences.append(new_seq)
+            new_sequence = [w]
+        else:
+            #continue old sequence
+            new_sequence.append(w)
+    sequences.append(new_sequence) #add the final sequence
+    return sequences
     
 def seq2orders(sequences):
-    a=1
+    #return binary orders from each sequence
+    orders = []
+    for seq in sequences:
+        for (i,p) in enumerate(seq):
+            orders.extend([[p,p2] for p2 in seq[i+1::]])
+    return orders
     
 def orders2clauses(orders, order_type = 'mixed'):
     a=1
