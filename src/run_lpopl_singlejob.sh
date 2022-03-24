02db5de310ebca1299a60ae26de2a57e2f1a49b1
jasonxyliu <xinyu_liu@brown.edu>
2022-03-24 19:43:10 -0400

    added mpi in bash

--- src/run_lpopl_singlejob.sh
+++ src/run_lpopl_singlejob.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #SBATCH -n 2
-#SBATCH --mem=298G
-#SBATCH -t 96:00:00
+#SBATCH --mem=98G
+#SBATCH -t 24:00:00
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e sbatch_out/job-%j.err
 #SBATCH -o sbatch_out/job-%j.out
+
+export PYTHONUNBUFFERED=TRUE
 
 algo="lpopl"
 task="no_orders"
@@ -13,7 +15,9 @@ train_size=10
 test_task="no_orders"
 map=0
 
-module load anaconda/3-5.2.0
+module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
 conda activate lpopl
-python3 run_experiments.py --algo=$algo --tasks=$task --train_size=$train_size --test_tasks=$test_task --map=$map
+module load mpi/openmpi_4.0.5_gcc_10.2_slurm20 gcc/10.2 cuda/11.1.1
+
+srun --mpi=pmix python -m mpi4py.futures run_experiments.py --algo=$algo --tasks=$task --train_size=$train_size --test_tasks=$test_task --map=$map
