2d66e784bf2482d515aacbf68221457e4b59042b
jasonxyliu <xinyu_liu@brown.edu>
2022-03-30 16:50:40 -0400

    removed mpi from lpopl training

--- src/run_lpopl_singlejob.sh
+++ src/run_lpopl_singlejob.sh
@@ -18,6 +18,5 @@ map=0
 module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
 conda activate lpopl
-module load mpi/openmpi_4.0.5_gcc_10.2_slurm20 gcc/10.2 cuda/11.1.1
 
-srun --mpi=pmix python -m mpi4py.futures run_experiments.py --algo=$algo --tasks=$task --train_size=$train_size --test_tasks=$test_task --map=$map
+python run_experiments.py --algo=$algo --tasks=$task --train_size=$train_size --test_tasks=$test_task --map=$map
