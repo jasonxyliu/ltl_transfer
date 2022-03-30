e8c6f2b849d4454a4d6b2d3115216d1520b0a66d
jasonxyliu <xinyu_liu@brown.edu>
2022-03-30 09:33:35 -0400

    updated relabel_cluster bash script
    corrected args needed for random ltls
    updated file path
    updated resource

--- src/run_relabel_cluster.sh
+++ src/run_relabel_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 96
 #SBATCH --mem=256G
-#SBATCH -t 10:00:00
+#SBATCH -t 99:00:00
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e sbatch_out/job-%j.err
@@ -10,13 +10,19 @@
 export PYTHONUNBUFFERED=TRUE
 
 algo="zero_shot_transfer"
-task="transfer_interleaving"
+train_type="soft_strict"
+train_size=50
+test_type="soft_strict"
 map=0
+run_id=0
+relabel_method="cluster"
 
 module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
 conda activate lpopl
 module load mpi/openmpi_4.0.5_gcc_10.2_slurm20 gcc/10.2 cuda/11.1.1
 
-srun --mpi=pmix python -m mpi4py.futures run_experiments.py --algorithm=$algo --tasks=$task --map=$map
-cp -r ../tmp/* ~/data/shared/ltl-transfer/tmp/
+srun --mpi=pmix python -m mpi4py.futures run_experiments.py --algo=$algo --tasks=$train_type --train_size=$train_size --test_tasks=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method
+cp -r ../tmp ~/data_gdk/shared/ltl-transfer/tmp_jason
+#cp -r ../tmp/* ~/data/shared/ltl-transfer/tmp/
+
