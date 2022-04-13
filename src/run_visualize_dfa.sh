6f2cbe51df58f8038e7a791b7b54ab3a8798e86f
jasonxyliu <xinyu_liu@brown.edu>
2022-04-12 21:11:43 -0400

    bug fix

--- src/run_visualize_dfa.sh
+++ src/run_visualize_dfa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -n 19
-#SBATCH --mem=19G
-#SBATCH -t 24:00:00
+#SBATCH -n 99
+#SBATCH --mem=199G
+#SBATCH -t 36:00:00
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e sbatch_out/job-%j.err
@@ -11,4 +11,4 @@ module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
 conda activate lpopl
 
-python visulaize_dfa_graph.py
+python visualize_dfa_graph.py
