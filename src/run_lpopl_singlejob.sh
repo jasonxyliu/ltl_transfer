997d0625fd9eda8a4ee6fb7de48cfb1d8e1220ea
jasonxyliu <xinyu_liu@brown.edu>
2022-04-24 22:57:26 -0400

    switched to mpi parallel

--- src/run_lpopl_singlejob.sh
+++ src/run_lpopl_singlejob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -n 64
+#SBATCH -n 199
 #SBATCH --mem=198G
-#SBATCH -t 96:00:00
+#SBATCH -t 99:00:00
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e sbatch_out/job-%j.err
@@ -10,11 +10,11 @@
 export PYTHONUNBUFFERED=TRUE
 
 algo="lpopl"
-train_type="no_orders"
+train_type="soft_strict"
 train_size=50
 test_type="no_orders"
 map=0
-train_steps=250000
+train_steps=50000
 
 module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
--- src/run_relabel_cluster.sh
+++ src/run_relabel_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -n 369
-#SBATCH --mem=199G
-#SBATCH -t 1:00:00
+#SBATCH -n 99
+#SBATCH --mem=99G
+#SBATCH -t 99:00:00
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e sbatch_out/job-%j.err
@@ -12,7 +12,7 @@ export PYTHONUNBUFFERED=TRUE
 algo="zero_shot_transfer"
 train_type="mixed"
 train_size=50
-test_type="mixed"
+test_type="no_orders"
 map=0
 run_id=0
 relabel_method="cluster"
@@ -21,9 +21,9 @@ edge_matcher="strict"
 module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
 conda activate lpopl
-#module load mpi/openmpi_4.0.5_gcc_10.2_slurm20 gcc/10.2 cuda/11.1.1
+module load mpi/openmpi_4.0.5_gcc_10.2_slurm20 gcc/10.2 cuda/11.1.1
 
-python run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher
-#srun --mpi=pmix python -m mpi4py.futures run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher
+#python run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher
+srun --mpi=pmix python -m mpi4py.futures run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher
 #cp -r ../tmp/* ~/data_gdk/shared/ltl-transfer/tmp/
 #cp -r ../results/* ~/data_gdk/shared/ltl-transfer/results/
