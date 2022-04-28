49a2be30592563a2b8405c9d6228b479af5d45bf
jasonxyliu <xinyu_liu@brown.edu>
2022-04-28 13:41:22 -0400

    changed results dname for debug

--- src/test_utils.py
+++ src/test_utils.py
@@ -68,7 +68,7 @@ class Tester:
                 else:
                     self.experiment = "%s_%d/map_%d" % (train_type, train_size, map_id)
                     self.tasks, self.transfer_tasks = read_train_test_formulas(train_type, test_type, train_size)
-                    self.transfer_results_dpath = os.path.join("../results", "%s_%d_%s_%s" % (train_type, train_size, test_type, edge_matcher), "map_%d" % map_id)
+                    self.transfer_results_dpath = os.path.join("../results_fix_infloop_jason", "%s_%d_%s_%s" % (train_type, train_size, test_type, edge_matcher), "map_%d" % map_id)
                 os.makedirs(self.transfer_results_dpath, exist_ok=True)
                 self.transfer_log_fpath = os.path.join(self.transfer_results_dpath, "zero_shot_transfer_log.txt")
                 logging.basicConfig(filename=self.transfer_log_fpath, filemode='w', level=logging.INFO, format="%(message)s")
