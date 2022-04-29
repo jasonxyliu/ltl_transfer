2e95405884a559fcdfdc36b911421590039551c0
jasonxyliu <xinyu_liu@brown.edu>
2022-04-29 10:05:00 -0400

    updated dname, arg for metric computing

--- src/test_utils.py
+++ src/test_utils.py
@@ -280,7 +280,7 @@ def transfer_metrics(train_type, train_size, test_type, map_id, num_times, edge_
     """
     Compute evaluation metrics for zero-shot transfer
     """
-    results_dpath = os.path.join("../results", "%s_%d_%s_%s" % (train_type, train_size, test_type, edge_matcher), "map_%d" % map_id)
+    results_dpath = os.path.join("../results_fix_infloop_jason", "%s_%d_%s_%s" % (train_type, train_size, test_type, edge_matcher), "map_%d" % map_id)
     results = read_json(os.path.join(results_dpath, "zero_shot_transfer_results.json"))
     task2success = results["task2success"]
     success_rates = []
@@ -392,7 +392,7 @@ if __name__ == "__main__":
                         help='This parameter indicated which tasks to solve. The options are: ' + str(train_types))
     parser.add_argument('--train_size', default=50, type=int,
                         help='This parameter indicated the number of LTLs in the training set')
-    parser.add_argument('--test_type', default='hard', type=str,
+    parser.add_argument('--test_type', default='soft', type=str,
                         help='This parameter indicated which test tasks to solve. The options are: ' + str(test_types))
     parser.add_argument('--map', default=0, type=int,
                         help='This parameter indicated which map to use. It must be a number between -1 and 9. Use "-1" to run experiments over the 10 maps, 3 times per map')
