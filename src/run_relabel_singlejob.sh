e6a8534a01924920f70d4b22eef575130c94aca4
jasonxyliu <xinyu_liu@brown.edu>
2022-05-06 10:59:47 -0400

    added comments for resources needed for relabel, transfer

--- src/run_relabel_singlejob.sh
+++ src/run_relabel_singlejob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -n 455
-#SBATCH --mem=99G
+#SBATCH -n 365
+#SBATCH --mem=199G
 #SBATCH -t 99:00:00
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
@@ -13,10 +13,10 @@ algo="zero_shot_transfer"
 train_type="mixed"
 train_size=50
 test_type="mixed"
-map=1
+map=5
+edge_matcher="relaxed"
 run_id=0
 relabel_method="cluster"
-edge_matcher="rigid"
 
 module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
@@ -27,3 +27,5 @@ module load mpi/openmpi_4.0.5_gcc_10.2_slurm20 gcc/10.2 cuda/11.1.1
 srun --mpi=pmix python -m mpi4py.futures run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher
 #cp -r ../tmp/* ~/data_gdk/shared/ltl-transfer/tmp/
 #cp -r ../results/* ~/data_gdk/shared/ltl-transfer/results/
+#relabel: 365 cores, 200G, 100hrs
+#transfer: 100 cores, 100G 90mins
