35de1562952d3848363627a68ea40aa526e3250c
ajshah91 <ankitshah91@gmail.com>
2022-05-06 17:07:09 -0400

    adding result figures

--- src/result_analyzer.py
+++ src/result_analyzer.py
@@ -11,9 +11,11 @@ from zero_shot_transfer import *
 import dill
 import numpy as np
 from scipy.stats import beta
+import pandas as pd
+import seaborn as sns
 
 RESULT_DPATH = '../results_test'
-
+rc = {'axes.labelsize': 28, 'axes.titlesize': 32, 'legend.fontsize': 24, 'xtick.labelsize': 24, 'ytick.labelsize': 22}
 
 class Record:
 
@@ -95,6 +97,248 @@ def get_success_CI(results, trials = 100, CI = 0.95):
     return success_CI
 
 
+def get_results(train_types, test_types, train_sizes, map_ids):
+    results = {}
+    for train_type in train_types:
+        for test_type in test_types:
+            for train_size in train_sizes:
+                for map_id in map_ids:
+                    record = Record(train_type, train_size, test_type, 'rigid')
+                    results[(train_type, train_size, test_type, 'rigid', map_id)] = record
+                    record = Record(train_type, train_size, test_type, 'relaxed')
+                    results[(train_type, train_size, test_type, 'relaxed', map_id)] = record
+    return results
+
+type_names = {'hard': 'Hard',
+              'soft': 'Soft',
+              'soft_strict': 'Strict Soft',
+              'no_orders': 'No-orders',
+              'mixed': 'Mixed'}
+
+
+def create_data_table(results):
+    data = {}
+    i = 0
+    #dataframe columns 'Train Set', 'Size', 'Test Set', 'Map', 'Transfer Method'
+
+    for k in results:
+        data[i] = {}
+        data[i]['Train Set']  = type_names[k[0]]
+        data[i]['Size'] = k[1]
+        data[i]['Test Set'] = type_names[k[2]]
+        data[i]['Transfer Method'] = 'General' if k[3] == 'relaxed' else 'Constrained'
+        data[i]['Map'] = k[4]
+        data[i]['Success Rate'] = np.mean(results[k].success)
+        data[i]['Successful'] = np.sum(results[k].success)
+        data[i]['Fails'] = len(results[k].success) - data[i]['Successful']
+        i = i+1
+    data = pd.DataFrame.from_dict(data, orient = 'index')
+    return data
+
+def lpopl_success_rates():
+    data = {}
+    data[0] = {'Train Set': 'Mixed', 'Test Set': 'Mixed','Size':5, 'Transfer Method': 'LPOPL', 'Map': 0, 'Success Rate': 0.02}
+    data[1] = {'Train Set': 'Mixed', 'Test Set': 'Mixed','Size':10, 'Transfer Method': 'LPOPL', 'Map': 0, 'Success Rate': 0.05}
+    data[2] = {'Train Set': 'Mixed', 'Test Set': 'Mixed','Size':15, 'Transfer Method': 'LPOPL', 'Map': 0, 'Success Rate': 0.07}
+    data[3] = {'Train Set': 'Mixed', 'Test Set': 'Mixed','Size':20, 'Transfer Method': 'LPOPL', 'Map': 0, 'Success Rate': 0.08}
+    data[4] = {'Train Set': 'Mixed', 'Test Set': 'Mixed','Size':30, 'Transfer Method': 'LPOPL', 'Map': 0, 'Success Rate': 0.1}
+    data[5] = {'Train Set': 'Mixed', 'Test Set': 'Mixed','Size':40, 'Transfer Method': 'LPOPL', 'Map': 0, 'Success Rate': 0.1}
+    data[6] = {'Train Set': 'Mixed', 'Test Set': 'Mixed','Size':50, 'Transfer Method': 'LPOPL', 'Map': 0, 'Success Rate': 0.12}
+    data = pd.DataFrame.from_dict(data, orient='index')
+    return data
+
+def plot_fig2():
+    '''line plot comparison with LPOPL'''
+    train_types = ['mixed']
+    test_types = ['mixed']
+    train_sizes = [5,10,15,20,30,40,50]
+    map_ids = [0]
+    CI = 0.95
+    data_ours = create_data_table(get_results(train_types, test_types, train_sizes, map_ids))
+    data_lpopl = lpopl_success_rates()
+    data = pd.concat([data_ours, data_lpopl], axis=0, ignore_index = True)
+    
+    means = data.groupby(['Size', 'Transfer Method']).mean()
+    means_ours = data_ours.groupby(['Size', 'Transfer Method']).mean()
+    
+    #Decide color palette and assign colors
+    palette = sns.color_palette('deep')
+    colors = {}
+    matchers = list(means.loc[means.index[0][0]].index)
+    matchers_ours = list(means_ours.loc[means_ours.index[0][0]].index)
+    
+    for (i,m) in enumerate(matchers):
+        colors[m] = palette[i%len(palette)]        
+    
+    data = pd.concat([data_ours, data_lpopl], axis=0, ignore_index = True)
+    
+    
+    with sns.plotting_context('poster', rc = rc):
+        plt.figure(figsize = [10,8])
+        #sns.lineplot(data = data, x = 'Size', y = 'Success Rate', hue = 'Transfer Method', style = 'Transfer Method', dashes = False, markers = True, hue_order = matchers)
+        
+        #Create the line plots
+        sns.lineplot(data = data, x = 'Size', y = 'Success Rate', hue = 'Transfer Method', style = 'Transfer Method', dashes = False, markers = True, hue_order = matchers)
+        
+        # Create the error bars
+        for m in matchers_ours:
+            mean_locs = means['Success Rate'][:,m]
+            
+            successes = data_ours.groupby(['Size','Transfer Method']).sum()['Successful'][:,m]
+            fails = data_ours.groupby(['Size','Transfer Method']).sum()['Fails'][:,m]
+            lo_q = 0.5 - CI/2
+            hi_q = 0.5 + CI/2
+            lo = mean_locs - np.array([beta.ppf(lo_q, s, f) for (s,f) in zip(successes, fails)])
+            hi = np.array([beta.ppf(hi_q, s, f) for (s,f) in zip(successes, fails)]) - mean_locs
+            err = np.array(list(zip(lo, hi))).transpose()
+            
+            plt.errorbar(mean_locs.index, mean_locs, err , fmt = 'none', ecolor = colors[m], capsize = 10, capthick = 2)
+        plt.ylim(0,1.1)
+        plt.xlim(0,55)
+        plt.xlabel('Training Set Size')
+        plt.legend(loc = 'center right')
+        plt.savefig('figures/fig_2.png',dpi = 400, bbox_inches = 'tight')
+
+
+
+
+def plot_fig3A():
+    '''Line plots for mixed to different training types for rigid edge matching'''
+    train_types = ['mixed']
+    test_types = ['hard','soft','soft_strict','no_orders','mixed']
+    train_sizes = [5,10,15,20,30,40,50]
+    map_ids = [0]
+    CI = 0.95
+    data = create_data_table(get_results(train_types, test_types, train_sizes, map_ids))
+    
+    data = data.loc[data['Transfer Method'] == 'Constrained']
+    #data_relaxed = data.loc[data['Transfer Method'] == 'General']
+    
+    means = data.groupby(['Size', 'Test Set']).mean()
+    #means_ours = data_ours.groupby(['Size', 'Test Set']).mean()
+    
+    #Decide color palette and assign colors
+    palette = sns.color_palette('deep')
+    colors = {}
+    test_types = list(means.loc[means.index[0][0]].index)
+    #$matchers_ours = list(means_ours.loc[means_ours.index[0][0]].index)
+    
+    for (i,m) in enumerate(test_types):
+        colors[m] = palette[i%len(palette)]        
+    
+    #data = pd.concat([data_ours, data_lpopl], axis=0, ignore_index = True)
+    
+    
+    with sns.plotting_context('poster', rc = rc):
+        plt.figure(figsize = [10,8])
+        #sns.lineplot(data = data, x = 'Size', y = 'Success Rate', hue = 'Transfer Method', style = 'Transfer Method', dashes = False, markers = True, hue_order = matchers)
+        
+        #Create the line plots
+        sns.lineplot(data = data, x = 'Size', y = 'Success Rate', hue = 'Test Set', style = 'Test Set', dashes = False, markers = True, hue_order = test_types)
+        
+        # Create the error bars
+        for m in test_types:
+            mean_locs = means['Success Rate'][:,m]
+            
+            successes = data.groupby(['Size','Test Set']).sum()['Successful'][:,m]
+            fails = data.groupby(['Size','Test Set']).sum()['Fails'][:,m]
+            lo_q = 0.5 - CI/2
+            hi_q = 0.5 + CI/2
+            lo = mean_locs - np.array([beta.ppf(lo_q, s, f) for (s,f) in zip(successes, fails)])
+            hi = np.array([beta.ppf(hi_q, s, f) for (s,f) in zip(successes, fails)]) - mean_locs
+            err = np.array(list(zip(lo, hi))).transpose()
+            
+            plt.errorbar(mean_locs.index, mean_locs, err , fmt = 'none', ecolor = colors[m], capsize = 10, capthick = 2)
+        plt.ylim(0,1.1)
+        plt.xlim(0,55)
+        plt.xlabel('Training Set Size')
+        plt.legend(loc = 'upper right')
+        plt.savefig('figures/fig_3a.png',dpi = 400, bbox_inches = 'tight')
+        
+def plot_fig3B():
+    'Line plots for mixed to set types for relaxed edge matching'
+    train_types = ['mixed']
+    test_types = ['hard','soft','soft_strict','no_orders','mixed']
+    train_sizes = [5,10,15,20,30,40,50]
+    map_ids = [0]
+    CI = 0.95
+    data = create_data_table(get_results(train_types, test_types, train_sizes, map_ids))
+    
+    data = data.loc[data['Transfer Method'] == 'General']
+    #data_relaxed = data.loc[data['Transfer Method'] == 'General']
+    
+    means = data.groupby(['Size', 'Test Set']).mean()
+    #means_ours = data_ours.groupby(['Size', 'Test Set']).mean()
+    
+    #Decide color palette and assign colors
+    palette = sns.color_palette('deep')
+    colors = {}
+    test_types = list(means.loc[means.index[0][0]].index)
+    #$matchers_ours = list(means_ours.loc[means_ours.index[0][0]].index)
+    
+    for (i,m) in enumerate(test_types):
+        colors[m] = palette[i%len(palette)]        
+    
+    #data = pd.concat([data_ours, data_lpopl], axis=0, ignore_index = True)
+    
+    
+    with sns.plotting_context('poster', rc = rc):
+        plt.figure(figsize = [10,8])
+        #sns.lineplot(data = data, x = 'Size', y = 'Success Rate', hue = 'Transfer Method', style = 'Transfer Method', dashes = False, markers = True, hue_order = matchers)
+        
+        #Create the line plots
+        sns.lineplot(data = data, x = 'Size', y = 'Success Rate', hue = 'Test Set', style = 'Test Set', dashes = False, markers = True, hue_order = test_types)
+        
+        # Create the error bars
+        for m in test_types:
+            mean_locs = means['Success Rate'][:,m]
+            
+            successes = data.groupby(['Size','Test Set']).sum()['Successful'][:,m]
+            fails = data.groupby(['Size','Test Set']).sum()['Fails'][:,m]
+            lo_q = 0.5 - CI/2
+            hi_q = 0.5 + CI/2
+            lo = mean_locs - np.array([beta.ppf(lo_q, s, f) for (s,f) in zip(successes, fails)])
+            hi = np.array([beta.ppf(hi_q, s, f) for (s,f) in zip(successes, fails)]) - mean_locs
+            err = np.array(list(zip(lo, hi))).transpose()
+            
+            plt.errorbar(mean_locs.index, mean_locs, err , fmt = 'none', ecolor = colors[m], capsize = 10, capthick = 2)
+        plt.ylim(0,1.1)
+        plt.xlim(0,55)
+        plt.xlabel('Training Set Size')
+        plt.legend(loc = 'lower right')
+        plt.savefig('figures/fig_3b.png',dpi = 400, bbox_inches = 'tight')
+
+def fig_4A():
+    '''heatmap for cross type rigid'''
+    test_types = ['hard','soft','soft_strict','no_orders','mixed']
+    train_types= test_types
+    train_sizes = [50]
+    map_ids = [0]
+    CI = 0.95
+    data = create_data_table(get_results(train_types, test_types, train_sizes, map_ids))
+    data = data.loc[data['Transfer Method']=='Constrained']
+    with sns.plotting_context('poster', rc = rc):
+        plt.figure(figsize = [12,10])
+        table = data.pivot_table(values = 'Success Rate', index='Train Set', columns='Test Set')
+        sns.heatmap(data = table, annot=True, vmin = 0, vmax = 1)
+        plt.savefig('figures/fig_4a.png',dpi = 400, bbox_inches = 'tight')
+    
+def fig_4B():
+    '''heatmap for cross type relaxed'''
+    test_types = ['hard','soft','soft_strict','no_orders','mixed']
+    train_types= test_types
+    train_sizes = [50]
+    map_ids = [0]
+    CI = 0.95
+    data = create_data_table(get_results(train_types, test_types, train_sizes, map_ids))
+    data = data.loc[data['Transfer Method']=='General']
+    with sns.plotting_context('poster', rc = rc):
+        plt.figure(figsize = [12,10])
+        table = data.pivot_table(values = 'Success Rate', index='Train Set', columns='Test Set')
+        sns.heatmap(data = table, annot=True, vmin = 0, vmax = 1)
+        plt.savefig('figures/fig_4b.png',dpi = 400, bbox_inches = 'tight')
+
+
 
 
 if __name__ == '__main__':
@@ -106,15 +350,16 @@ if __name__ == '__main__':
     test_types = ['mixed']
     train_sizes = [5,10,15,20,30,40,50]
     map_ids = [0]
-    results = {}
+    results = get_results(train_types, test_types, train_sizes, map_ids)
+    
 
-    for train_type in train_types:
-        for test_type in test_types:
-            for train_size in train_sizes:
-                for map_id in map_ids:
-                    record = Record(train_type, train_size, test_type, 'rigid')
-                    results[(train_type, train_size, test_type, 'rigid', map_id)] = record
-                    record = Record(train_type, train_size, test_type, 'relaxed')
-                    results[(train_type, train_size, test_type, 'relaxed', map_id)] = record
+    # for train_type in train_types:
+    #     for test_type in test_types:
+    #         for train_size in train_sizes:
+    #             for map_id in map_ids:
+    #                 record = Record(train_type, train_size, test_type, 'rigid')
+    #                 results[(train_type, train_size, test_type, 'rigid', map_id)] = record
+    #                 record = Record(train_type, train_size, test_type, 'relaxed')
+    #                 results[(train_type, train_size, test_type, 'relaxed', map_id)] = record
 
-    get_success_CI = get_success_CI(results)
+    # get_success_CI = get_success_CI(results)
