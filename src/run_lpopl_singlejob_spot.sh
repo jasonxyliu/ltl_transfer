33eab20e2a2745372d86597988028bd134d05a05
jasonxyliu <xinyu_liu@brown.edu>
2022-05-14 18:22:58 -0400

    bug fix
    currently spot domain only has 1 train, test type: hard

--- src/run_lpopl_singlejob_spot.sh
+++ src/run_lpopl_singlejob_spot.sh
@@ -12,6 +12,7 @@ export PYTHONUNBUFFERED=TRUE
 algo="lpopl"
 train_type="hard"
 train_size=2
+test_type="hard"
 map=20
 total_steps=200000
 incremental_steps=150000
@@ -22,4 +23,4 @@ module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
 conda activate lpopl
 
-python run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --map=$map --total_steps=$total_steps --incremental_steps=$incremental_steps --edge_matcher=$edge_matcher --dataset_name=$dataset_name
+python run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --total_steps=$total_steps --incremental_steps=$incremental_steps --edge_matcher=$edge_matcher --dataset_name=$dataset_name
