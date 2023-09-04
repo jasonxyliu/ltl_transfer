5666130cdd7c4ec8ad5f756b1a3275b6b6976ee1
Jason Liu <xinyu_liu@brown.edu>
2023-09-04 18:01:56 -0400

    updated helper

--- src/get_spot_state.py
+++ src/get_spot_state.py
@@ -1,12 +1,17 @@
 """
 Get robot pose in vision frame. Used to discretize environment.
 """
-
+import argparse
 import bosdyn.client
 
 if __name__ == '__main__':
+    # python3 src/get_spot_state.py --robot_name=tusker
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--robot_name", type=str, required=True, help="Name of the robot to operate.")
+    args = parser.parse_args()
+
     sdk = bosdyn.client.create_standard_sdk('show_robot_state')
-    robot = sdk.create_robot('gouger')
+    robot = sdk.create_robot(args.robot_name)
     robot.authenticate('user', 'bigbubbabigbubba')
     state_client = robot.ensure_client('robot-state')
     transforms_snapshot = state_client.get_robot_state().kinematic_state.transforms_snapshot
