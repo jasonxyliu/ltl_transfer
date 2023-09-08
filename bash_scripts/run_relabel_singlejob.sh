ac74adf2e9584eec225d7d7fd9ff975e55356e52
jasonxyliu <xinyu_liu@brown.edu>
2023-09-08 19:47:35 -0400

    request proper resources on cluster
    seff -j <JOBID>

--- bash_scripts/run_relabel_singlejob.sh
+++ bash_scripts/run_relabel_singlejob.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#SBATCH -n 365
-#SBATCH --mem=199G
+#SBATCH -N 3
+#SBATCH -n 144
+#SBATCH --mem=20G
 #SBATCH -t 99:00:00
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e sbatch_out/job-%j.err
 #SBATCH -o sbatch_out/job-%j.out
+
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_90
+#SBATCH --mail-user=xinyu_liu@brown.edu
 
 export PYTHONUNBUFFERED=TRUE
 
@@ -14,6 +18,7 @@ train_type="mixed"
 train_size=50
 test_type="mixed"
 map=5
+prob=0.9
 edge_matcher="relaxed"
 run_id=0
 relabel_method="cluster"
@@ -22,11 +27,9 @@ save_dpath="$HOME/data/shared/ltl-transfer"
 module load anaconda/2022.05
 source /oscar/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
 conda activate ltl_transfer
-module load mpi/openmpi_4.0.7_gcc_10.2_slurm22 gcc/10.2 cuda/11.7.1
+module load mpi/openmpi_4.0.7_gcc_10.2_slurm22
 
-#python src/run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher --save_dpath=$save_dpath
-srun --mpi=pmix python -m mpi4py.futures src/run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher --save_dpath=$save_dpath
-#cp -r ../tmp/* ~/data_gdk/shared/ltl-transfer/tmp/
-#cp -r ../results/* ~/data_gdk/shared/ltl-transfer/results/
-#relabel: 365 cores, 200G, 100hrs
-#transfer: 100 cores, 100G 90mins
+# python src/run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher --save_dpath=$save_dpath
+srun --mpi=pmix python -m mpi4py.futures src/run_experiments.py --algo=$algo --train_type=$train_type --train_size=$train_size --test_type=$test_type --map=$map --prob=$prob --run_id=$run_id --relabel_method=$relabel_method --edge_matcher=$edge_matcher --save_dpath=$save_dpath
+# relabel: 365 cores, 200G, 100hrs
+# transfer: 100 cores, 100G 90mins
